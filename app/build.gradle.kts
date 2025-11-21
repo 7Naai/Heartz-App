936382f8c0180d95637f379edf6fa15e4811e9ea
7Naai <iangomezchacon@gmail.com>
2025-11-21 18:16:55 -0300

    Se agregaron las dependencias de retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,5 +94,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.ui:ui-text:1.6.8")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
 }
